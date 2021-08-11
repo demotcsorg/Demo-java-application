f75b83c24e676e0b1f6f689859bf85ab08960129
Saurabh Rai <42387065+SaurabhRai19@users.noreply.github.com>
2021-08-11 09:57:45 +0530

    Update SpringwebApplicationTests.java

--- src/test/java/com/example/demo/SpringwebApplicationTests.java
+++ src/test/java/com/example/demo/SpringwebApplicationTests.java
@@ -15,7 +15,7 @@ class SpringwebApplicationTests {
 	public void testNameDailyCodeBuffer()
 	{
 		Messages obj = new Messages();
-		Assertions.assertEquals("Hello Daily Code Buffer!", obj.getMessage("Daily Code Buffer"));
+		Assertions.assertEquals("Hello Daily Code Buffer!", obj.getMessage("Daily Buffer"));
 	}
 
 }
