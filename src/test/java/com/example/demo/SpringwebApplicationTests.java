6dc061266bfd1eff02643823dc92c1403e782972
Saurabh Rai <42387065+SaurabhRai19@users.noreply.github.com>
2021-08-11 09:55:28 +0530

    Update SpringwebApplicationTests.java

--- src/test/java/com/example/demo/SpringwebApplicationTests.java
+++ src/test/java/com/example/demo/SpringwebApplicationTests.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
 
 @SpringBootTest
 class SpringwebApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testNameDailyCodeBuffer()
+	{
+		Messages obj = new Messages();
+		Assertions.assertEquals("Hello Daily Code Buffer!", obj.getMessage("Daily Code Buffer"));
 	}
 
 }
